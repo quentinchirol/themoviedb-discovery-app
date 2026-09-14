4ae2692b2d4623ee0313344615a0825becdc1704
quentinchirol <157401619+quentinchirol@users.noreply.github.com>
2026-09-14 14:42:32 +0000

    chore: add husky and commitlint to improve commit message management

--- src/back-end/config.ts
+++ src/back-end/config.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 // Récupérer le token d'accès à l'API TMDB depuis les variables d'environnement
 const tmdbAccessToken: string | undefined = process.env.TMDB_ACCESS_TOKEN;
-if (!tmdbAccessToken) {
-throw new Error(
-"TMDB_ACCESS_TOKEN is not defined in the environment variables.",
-);
+    if (!tmdbAccessToken) {
+        throw new Error(
+            "TMDB_ACCESS_TOKEN is not defined in the environment variables.",
+        );
 }
 export { tmdbAccessToken };
