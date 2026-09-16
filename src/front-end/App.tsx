d6b1ca86953895063ef34c0fbf7334d9f7541165
quentinchirol <157401619+quentinchirol@users.noreply.github.com>
2026-09-16 15:44:27 +0000

    feat: ✨ refactor movie list rendering to use MovieItem component

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import type { Movie } from "../back-end/schemas/MoviesTypes"
+import MovieItem from "./components/MovieItem"
 
 export default function App() {
     useEffect(() => {
@@ -28,18 +29,13 @@ export default function App() {
    return (
     <div>
       <h1>Popular Movies</h1>
-      {movies ? (
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>
-              <h2>{movie.title}</h2>
-              <p>{movie.overview}</p>
-              <p>Release Date: {movie.release_date}</p>
-              <p>Rating: {movie.vote_average}</p>
-            </li>
-          ))}
-        </ul>
-      ) : null}
+        {movies ? (
+            <ul>
+            {movies.map((movie) => (
+                <MovieItem key={movie.id} movie={movie} />
+            ))}
+            </ul>
+        ) : null}
     </div>
   )
 }
