f54dcbfb089a66e2f723c30fa19544b0f8ce50bc
quentinchirol <157401619+quentinchirol@users.noreply.github.com>
2026-09-23 15:26:25 +0000

    feat: ✨ add query parameters for fetching popular movies from TMDB API

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -5,6 +5,7 @@ import type {
   MoviesApiResponse,
   TmdbMoviesRawResponse
 } from './schemas/MoviesTypes';
+import { DEFAULT_LANGUAGE, DEFAULT_PAGE, DEFAULT_REGION } from './constants';
 
 // Create a new express application instance
 const app = express();
@@ -22,12 +23,25 @@ app.listen(port, () => {
 // Define a route handler for fetching popular movies from TMDB API
 app.get('/api/movies/popular', async (_req: express.Request, res: express.Response) => {
     try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/popular', {
-        headers: {
+        // Create a URLSearchParams object to build the query string for the TMDB API request
+      const queryParams = new URLSearchParams();
+
+      // Extract query parameters from the request and append them to the query string
+      const { language, page, region } = _req.query;
+
+      queryParams.append('language', (language as string) || DEFAULT_LANGUAGE);
+      queryParams.append('page', (page as string) || DEFAULT_PAGE);
+      queryParams.append('region', (region as string) || DEFAULT_REGION);
+
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?${queryParams.toString()}`,
+        {
+          headers: {
             Authorization: `Bearer ${tmdbAccessToken}`,
-            'Content-Type': 'application/json;charset=utf-8'
-        }
-    });
+            'Content-Type': 'application/json;charset=utf-8',
+          },
+        },
+      );
     if (!response.ok) {
         throw new Error(`TMDB API request failed with status ${response.status}`);
     }
